a8070e7dfe378cbe4c82a7766be719e87997ee30
Hard-Luck <tyreemarka@gmail.com>
2022-07-20 15:51:02 +0100

    Screen is updating

--- index.js
+++ index.js
@@ -15,4 +15,19 @@ function evaluateString(string) {
   }
 }
 
-//add event handlers
+const buttons = document.querySelectorAll(".button");
+console.log(buttons);
+let total = 0;
+// initialize screen text
+let screen = document.querySelector(".screen");
+screen.innerText = total.toString();
+let screenString = "";
+
+function updateScreen(x) {
+  x.addEventListener("click", (e) => {
+    screenString += e.target.attributes["data-value"].value;
+    screen.innerText = screenString.toString();
+  });
+}
+
+buttons.forEach((button) => updateScreen(button));
