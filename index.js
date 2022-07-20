4c644a9c58c993db9d14f9d02acbb78cdc92dd2c
Hard-Luck <tyreemarka@gmail.com>
2022-07-20 12:55:34 +0100

    Set up the the html to look like a calculator

---
+++ index.js
@@ -0,0 +1,16 @@
+function evaluateString(string) {
+  stringSplit = string.split(" ");
+
+  switch (stringSplit[1]) {
+    case "+":
+      return +stringSplit[0] + +stringSplit[2];
+    case "-":
+      return +stringSplit[0] - +stringSplit[2];
+    case "*":
+      return +stringSplit[0] * +stringSplit[2];
+    case "x":
+      return +stringSplit[0] * +stringSplit[2];
+    case "/":
+      return (+stringSplit[0] / +stringSplit[2]).toFixed(3);
+  }
+}
